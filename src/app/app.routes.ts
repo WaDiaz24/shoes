d783877f86ea4c1da492aca4591aebf773d55d83
Andrés Díaz <diazandres756@gmail.com>
2024-12-11 12:09:42 -0500

    Se realiza el home con el footer y header en diferentes componentes. También se realiza configuración de las rutas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './app/pages/home/home.component';
+import { ProductsComponent } from './app/pages/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: '**', redirectTo: '' },
+];
