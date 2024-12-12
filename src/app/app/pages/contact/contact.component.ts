4e38d3aadf4008cdcffdf3d0aa11dee427923574
Andrés Díaz <diazandres756@gmail.com>
2024-12-12 11:35:08 -0500

    Se implemento la pagina de contacto. Con un formulario donde el cliente mande su mensaje para poder contactar. Aun falta funcionalidad.

---
+++ src/app/app/pages/contact/contact.component.ts
@@ -0,0 +1,13 @@
+import { Component, Pipe } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+
+}
