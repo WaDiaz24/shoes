0f709f99887157b84e3d62b5ba38b02bbccb318a
Andrés Díaz <diazandres756@gmail.com>
2024-12-12 11:19:49 -0500

    Se implemento la página de productos, con los diferentes productos con nombre, su imagen y el boton de poder comprar.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './app/pages/home/home.component';
 import { ProductsComponent } from './app/pages/products/products.component';
+import { PurchaseComponent } from './app/pages/purchase/purchase.component';
+import { ContactComponent } from './app/pages/contact/contact.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
+  {path: 'purchase', component: PurchaseComponent},
+  {path: 'contact', component: ContactComponent},
   { path: '**', redirectTo: '' },
+ 
 ];
