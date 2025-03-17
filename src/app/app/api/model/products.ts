425b1069b3d53d51a9498ec80f0cc897fb0a849a
Andrés Díaz <diazandres756@gmail.com>
2025-03-17 18:19:51 -0500

    Se trabajo que en la página de venta se traiga los detalles del producto dinámicamente.

--- src/app/app/api/model/products.ts
+++ src/app/app/api/model/products.ts
@@ -9,7 +9,7 @@ export interface Product {
   price: number;
   discount: number;
   stock: number;
-
+  color: string
   image: string;
 }
 
--- src/app/app/pages/purchase/purchase.component.ts
+++ src/app/app/pages/purchase/purchase.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Product } from '../../api/model/products';
+import { PriceCalculatorService } from '../../../services/price-calculator.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-purchase',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './purchase.component.html'
 })
 export class PurchaseComponent{
-  product: any;
+  product?: Product;
 
-  constructor(private readonly router: Router){
+  constructor(private readonly router: Router, private readonly  priceCalculator: PriceCalculatorService){
     const navigation = this.router.getCurrentNavigation();
     this.product = navigation?.extras.state?.['product'] || null;
+  }
+
+  getDiscountPrice(price: number, discount: number): number {
+    return this.priceCalculator.calculateDiscountedPrice(price, discount);
   }
 
 }
